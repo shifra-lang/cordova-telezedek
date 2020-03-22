ae1452d4107616c1a954f6cdecccc365731ecdff
Lang <pb11@szmc.org.il>
2020-03-22 10:05:01 +0200

    initial commit

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-fcm-with-dependecy-updated.FCMPlugin",
+      "file": "plugins/cordova-plugin-fcm-with-dependecy-updated/www/FCMPlugin.js",
+      "pluginId": "cordova-plugin-fcm-with-dependecy-updated",
+      "clobbers": [
+        "FCMPlugin"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-fcm-with-dependecy-updated": "4.2.0",
+    "cordova-plugin-whitelist": "1.3.4"
+  };
+});
